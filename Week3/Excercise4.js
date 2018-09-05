53d563f5e00723117484e33dfdbee477e6027347
Nsamudera <nsamudera12@gmail.com>
2018-09-05 11:41:45 +0700

    Add description for Excercise4.js

--- Week3/Excercise4.js
+++ Week3/Excercise4.js
@@ -7,13 +7,13 @@ var school = 'SMA International Metro'
 
 function dataHandling2 (){
     //Splice Section
-    input.splice(1,1,input[1] + lastname)
-    input.splice(2,1,'Provinsi ' + input[2])
-    input.splice(4,1,gender,school)
+    input.splice(1,1,input[1] + lastname) // To remove 1 array starting from the first index and then add the first index + 'last name'
+    input.splice(2,1,'Provinsi ' + input[2]) //To remove 1 array starting from the second index and then to add the word "Provinsi" + the second index
+    input.splice(4,1,gender,school)// To remove 1 array starting from the fourth index and to add 'gender' and 'school'
     console.log(input)
     //Date split - Month
-    var datesplit=input[3].split("/")
-    switch (datesplit[1]){
+    var datesplit=input[3].split("/")//To split the date array (which is in the third index) 
+    switch (datesplit[1]){ //To display the month name (depending on what is in the first index of 'datesplit')
         case '01':
         console.log('Januari');
         break;
@@ -52,7 +52,7 @@ function dataHandling2 (){
         break;
 }
 //Descending sort of datesplit
-datesplit.sort(function(value1, value2) {return Number(value1)<Number(value2)})
+datesplit.sort(function(value1, value2) {return Number(value1)<Number(value2)}) //To sort in descending order (but to based it on number rather than string)
 console.log(datesplit)
 
 //date using hyphens
@@ -60,7 +60,7 @@ datesplit=input[3].split("/") //to change date split back to original form
 console.log(datesplit.join("-"))
 
 //slice name
-console.log(input[1].slice(0,15))
+console.log(input[1].slice(0,15))// To slice the name (index 1 of the array) from index 0 to index 14 (15 characters)
 }
 
 dataHandling2()
