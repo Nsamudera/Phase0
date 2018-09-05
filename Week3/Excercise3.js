8e3ec8b5eb6c7bb85b188b90b3cef1c2fb6dbee8
Nsamudera <nsamudera12@gmail.com>
2018-09-05 11:22:20 +0700

    Add description in Excercise3.js

--- Week3/Excercise3.js
+++ Week3/Excercise3.js
@@ -8,11 +8,12 @@ var input = [
 ]
 
 function dataHandling(){
+    //for each instance that the number of array (variable 'n) is less than the length of the input array then . . . 
     for(var n=0; n<input.length; n++){
-        console.log('Nomor ID: ' + input[n][0])
-        console.log('Nama Lengkap: ' + input[n][1])
-        console.log('TTL: ' + input[n][2] + ' ' + input[n][3])
-        console.log('Hobi: ' + input[n][4])
+        console.log('Nomor ID: ' + input[n][0]) // Display the ID number of the current array (index 0)
+        console.log('Nama Lengkap: ' + input[n][1]) // Display the name of the current array (index 1)
+        console.log('TTL: ' + input[n][2] + ' ' + input[n][3]) // Display the location and birth date of the current array (index 2 and 3)
+        console.log('Hobi: ' + input[n][4]) // Display the hobby of the current array (index 4)
         console.log('\n') // to add space between array inputs
     }
 }
